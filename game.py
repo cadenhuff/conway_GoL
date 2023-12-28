a91be24d90af1c9723b691c8bf8450484cf56a3b
Caden Huffman <cadenhuf@bu.edu>
2023-12-27 21:17:54 -0500

    Board is working, starting work on game logic

--- game.py
+++ game.py
@@ -5,8 +5,9 @@ import numpy as np
 pygame.init()
 
 # Constants
-width, height = 800, 600
-cell_size = 20
+width, height = 1200, 700
+#800,600
+cell_size = 25
 rows, cols = height // cell_size, width // cell_size
 border_size = 1
 
@@ -15,7 +16,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Create the initial grid with random values
-grid = np.random.choice([0], size=(rows, cols))
+grid = np.zeros((rows, cols), dtype=int)
 
 # Create Pygame screen
 screen = pygame.display.set_mode((width, height))
@@ -36,35 +37,26 @@ def update_grid():
                     new_grid[row, col] = 1
 
 
+# Function to update the grid based on user input
+def toggle_grid(mouse_pos):
+    print(mouse_pos)
+    col = mouse_pos[0] // cell_size
+    row = mouse_pos[1] // cell_size
 
+    # Toggle the state of the clicked cell
+    grid[row, col] = 1 - grid[row, col]
 
-
-
-# Game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Copy the grid to make updates simultaneously
-    new_grid = grid.copy()
-
-    # Update the grid based on Conway's rules
-    update_grid()
-
-    # Update the grid
-    grid = new_grid
-
-    # Draw the grid
-    screen.fill(BLACK)
+def draw_grid():
+    screen.fill(WHITE)
     for row in range(rows):
         for col in range(cols):
             x = col * cell_size
             y = row * cell_size
 
             # Draw cell
-            pygame.draw.rect(screen, WHITE, (x, y, cell_size, cell_size))
+            if grid[row,col] == 1:
+                pygame.draw.rect(screen, BLACK, (x, y, cell_size, cell_size))
+            
 
             # Draw border
             pygame.draw.rect(screen, BLACK, (x, y, cell_size, cell_size), border_size)
@@ -74,6 +66,41 @@ while running:
 
     # Adjust the frame rate
     pygame.time.delay(100)
+
+
+# Game loop
+running = True
+game_running = False
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Left mouse button
+                toggle_grid(pygame.mouse.get_pos())
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                game_running = True
+
+
+
+    # Copy the grid to make updates simultaneously
+    new_grid = grid.copy()
+    while game_running:
+        new_grid = grid.copy()
+        #Update the grid based on Conway's rules
+        update_grid()
+        if (np.array_equal(grid,new_grid)):
+            game_running = False
+        grid = new_grid
+        draw_grid()
+    # Update the grid
+    
+    grid = new_grid
+    
+
+    # Draw the grid
+    draw_grid()
 
 # Quit Pygame
 pygame.quit()
